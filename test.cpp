3c28cf53913c2da7a26b9e228f8b1eef8bcf71d6
Rolingad <36553324+Rolingad@users.noreply.github.com>
2018-02-17 18:01:46 -0500

    update test // two functions
    second function's test not implemented

--- test.cpp
+++ test.cpp
@@ -5,8 +5,8 @@
  * Project 3: 0h h1 Test Suite
  * Winter 2018
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * <#Nathan Fialkoff and Mary Reiber#>
+ * <#npfialk#>
  *
  * <#A description of the project here#>
  */
@@ -21,6 +21,9 @@ void test_count_unknown_squares();
 
 int main() {
     test_count_unknown_squares();
+    cout << endl;
+
+    test_row_has_no_threes_of_color();
 
     // add your calls to test functions here
 
@@ -41,6 +44,19 @@ void test_count_unknown_squares() {
     cout << count_unknown_squares(board, size_1) << endl;
 
     // add more tests here
+}
+
+void test_row_has_no_threes_of_color() {
+    int board[MAX_SIZE][MAX_SIZE];
+
+    //test case 1
+    string test_board_1[] = { "XO-O",
+        "--OO",
+        "---X",
+        "--O-" };
+    string test_board_2[] = { }
+
+    row_has_no_threes_of_color(board, 4, 3, BLUE);
 }
 
 // define more test functions here
